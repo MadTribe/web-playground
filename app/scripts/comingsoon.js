934bdb66de94325d848c2fb0129476e416ca5f8e
MadTribe <MadTribe@users.noreply.github.com>
2017-01-02 21:56:28 +0800

    added brag animations

--- app/scripts/comingsoon.js
+++ app/scripts/comingsoon.js
@@ -5,4 +5,11 @@ setTimeout( function() {
   //$('.message').addClass('swingdrop');
   console.log('dropping');
   document.getElementsByClassName("message")[0].classList.add('swingdrop');
+  setTimeout( function() {
+      document.getElementsByClassName("humblebrag1")[0].classList.add('visible');
+      setTimeout( function() {
+          document.getElementsByClassName("humblebrag2")[0].classList.add('visible');
+      }, 1000);
+
+  }, 1000);
 },1000);
